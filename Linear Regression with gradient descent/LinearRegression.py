baa966e33dea7ddf633eb849161cc7b4a24d85b0
hbchawla <deepak.chawla@bridgelabz.com>
2018-06-15 19:36:07 +0530

    Improvement in the code.

--- Linear Regression with gradient descent/LinearRegression.py
+++ Linear Regression with gradient descent/LinearRegression.py
@@ -1,6 +1,8 @@
 # import necessary modules
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import style
 
 
 df = pd.read_csv('iris.csv')
@@ -21,7 +23,7 @@ y_data_test = list(pd.DataFrame(test, columns=['sepal_width'])['sepal_width'])
 l_rate = 0.0001
 
 # Total iterations
-iterations = 1000
+iterations = 1100
 
 # length of the train and test datasets.
 m = len(x_data_train)
@@ -30,16 +32,23 @@ n = len(y_data_test)
 
 # Function gradient_descent() is calculate the theta0 and theta1 for hypothesis of linear regression.
 def gradient_descent():
-    newtheta0 = newtheta1 = temp1 = temp2 = theta0 = theta1 = i = 0
+    temp1 = temp2 = i = 0
+    tempnewtheta0 = 0
+    tempnewtheta1 = 1
+    newtheta0 = 0
+    newtheta1 = 1
     y_pred = []
 
     # iterations loop then assign theta0 and theta1 to newtheta0 and newtheta1 variables.
     while i < iterations:
-        for j in range(m):
-            temp1 += ((theta0 + theta1 * x_data_train[j]) - (y_data_train[j]))
-            temp2 += ((theta0 + theta1 * x_data_train[j]) - (y_data_train[j]) * x_data_train[j])
-        newtheta0 += (newtheta0 - (l_rate * (1 / m) * temp1))
-        newtheta1 += (newtheta1 - (l_rate * (1 / m) * temp2))
+        for j in range(0, m):
+            temp1 += (abs(newtheta0 + newtheta1 * x_data_train[j]) - (y_data_train[j]))
+            temp2 += (abs(newtheta0 + newtheta1 * x_data_train[j]) - (y_data_train[j]) * x_data_train[j])
+        tempnewtheta0 = (tempnewtheta0 + abs(tempnewtheta0 - ((l_rate * temp1)/m)))
+        tempnewtheta1 = (tempnewtheta1 + abs(tempnewtheta1 - ((l_rate * temp2)/m)))
+        if not np.isinf(tempnewtheta1):
+            newtheta0 = tempnewtheta0
+            newtheta1 = tempnewtheta1
         i += 1
     # predict the values by giving x_input_test.
     for i in range(len(x_data_test)):
@@ -49,6 +58,7 @@ def gradient_descent():
 
     # call error_calculate()
     print("Accuracy in model is",  avgerror(y_pred))
+    # graph(y_pred)
 
 
 # Function error_calculate() is calculate the accuracy of the predicted values with the input y test values.
@@ -57,6 +67,12 @@ def avgerror(y_pred_test):
     for i in range(0, n):
         total_error += abs(y_pred_test[i] - y_data_test[i])
     return 1 - total_error/len(y_data_test)
+
+
+def graph(y_pred):
+    plt.scatter(x_data_train, y_data_train, color='g', label='whole data')
+    plt.plot(x_data_test, y_pred, color='r', label='predicted value')
+    plt.show()
 
 
 if __name__ == '__main__':

d5f09d7610ddde567e23ea266ec6e1befe7cec02
hbchawla <deepak.chawla@bridgelabz.com>
2018-06-17 16:03:58 +0530

    Improvement in the code.

--- Linear Regression with gradient descent/LinearRegression.py
+++ Linear Regression with gradient descent/LinearRegression.py
@@ -3,18 +3,17 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import style
+from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv('iris.csv')
 
 # Assign the split data into train and test variables.
-x_data_length = np.array(pd.DataFrame(df, columns=['sepal_length'])['sepal_length'])
-y_data_width = np.array(pd.DataFrame(df, columns=['sepal_width'])['sepal_width'])
+x_data_set = np.array(pd.DataFrame(df, columns=['sepal_length'])['sepal_length'])
+y_data_set = np.array(pd.DataFrame(df, columns=['sepal_width'])['sepal_width'])
 
-x_data_train = x_data_length[:90]
-x_data_test = x_data_length[91:152]
-
-y_data_train = y_data_width[:90]
-y_data_test = y_data_width[91:152]
+x_data_train, x_data_test, y_data_train, y_data_test = train_test_split(
+        x_data_set, y_data_set, test_size=0.25, shuffle=False)
 
 # Learning Rate
 l_rate = 0.0001
@@ -41,13 +40,11 @@ def gradient_descent():
         newtheta0 = newtheta0 - ((l_rate * temp1)/m)
         newtheta1 = newtheta1 - ((l_rate * temp2)/m)
         i += 1
-    # print(newtheta0,newtheta1)
     # predict the values by giving x_input_test.
     for i in range(len(x_data_test)):
         temp = (newtheta0 + newtheta1 * x_data_test[i])
         temp = float(str(temp)[0:3])
         y_pred = np.append(y_pred, temp)
-    print(y_pred)
     # call error_calculate()
     print("Accuracy in model is",  avgerror(np.asarray(y_pred)))
     # graph(y_pred)

6cdce99658b0745c735fa3fdc225d842e85e9205
hbchawla <deepak.chawla@bridgelabz.com>
2018-07-03 11:40:43 +0530

    Improvement in th code.

--- Linear Regression with gradient descent/LinearRegressionSklearn.py
+++ Linear Regression with gradient descent/LinearRegressionSklearn.py
@@ -5,13 +5,18 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('iris.csv')
-features = ['sepal_length', 'sepal_width', 'petal_width']
-predicted_feature = ['petal_length']
+# df = pd.read_csv('iris.csv')
+# features = ['sepal_length', 'sepal_width', 'petal_width']
+# predicted_feature = ['petal_length']
 
 # df = pd.read_csv('petrol_consumption.csv')
 # features = ['Petrol_tax', 'Average_income', 'Paved_Highways', 'Population_Driver_licence(%)']
 # predicted_feature = ['Petrol_Consumption']
+
+
+df = pd.read_csv('final_data.csv')
+features = ['bathrooms', 'bedrooms', 'finishedsqft', 'totalrooms']
+predicted_feature = ['price']
 
 
 X = pd.DataFrame(df, columns=features)
@@ -36,5 +41,4 @@ for i in range(0, len(y_data_test)):
 total_error = (total_error/len(y_data_test))
 accuracy = 1 - total_error
 
-print(y_data_test, y_pred_test)
 print(mean_squared_error(y_data_test, y_pred_test), np.sqrt(mean_squared_error(y_data_test, y_pred_test)), accuracy*100)
